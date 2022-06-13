ca17137d1143ab75a049a8520e9e9f2b0a00d09e
cexll <evanxian9@gmail.com>
2022-06-13 15:05:12 +0800

    update

--- sh/install_ttf.sh
+++ sh/install_ttf.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-mkdir /usr/share/fonts
+echo 'Installing ttf'
+if [ ! -d "/usr/share/fonts" ]; then
+  mkdir /usr/share/fonts
+fi
 curl -sSL https://cdn.jsdelivr.net/gh/farmkd/static/simsun.ttc -o /usr/share/fonts/simsun.ttc
